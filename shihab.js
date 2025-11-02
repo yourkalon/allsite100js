429b07a86c146fc1ea65d355a371ea6dbfe28d7a
yourkalon000 <165013795+yourkalon000@users.noreply.github.com>
2025-11-03 04:26:55 +0800

    Create shihab.js

---
+++ shihab.js
@@ -0,0 +1,5 @@
+// প্লে বাটন লুকাও, স্পিনার দেখাও
+    playButton.style.display = 'none';
+    spinner.style.display = 'block';
+
+    const redirectURL = "https://zmistar.blogspot.com/p/parves_9.html";
