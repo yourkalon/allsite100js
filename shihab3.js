e4ada9b2c19fa714c61b116bbe16e5211dbe9e0a
yourkalon <165014118+yourkalon@users.noreply.github.com>
2025-11-04 13:25:47 +0800

    Update shihab3.js

--- shihab3.js
+++ shihab3.js
@@ -6,7 +6,7 @@
     playButton.style.display = 'none';
     spinner.style.display = 'block';
 
-    const redirectURL = "https://wards.short.gy/P0poxG";
+    const redirectURL = "https://sssigd.short.gy/Nb9TuP";
 
     // 🔹 ০.৫ সেকেন্ড পর নতুন ট্যাবে লিঙ্ক ওপেন হবে
     setTimeout(() => {
